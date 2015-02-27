7ed9eb384db995ea532f0e71948d073fe97d5c25
Austin Luttrell <aluttrell@harding.edu>
2015-02-26 20:38:02 -0600

    Added The ability to read in from a text file
    P.S. I really should have made this more than one commit... sorry

--- Platformer/Platformer/Game1.cs
+++ Platformer/Platformer/Game1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -8,6 +9,8 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics;
 
 namespace Platformer
 {
@@ -16,13 +19,24 @@ namespace Platformer
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        const int LEVEL_WIDTH = 89;
+        const int LEVEL_HEIGHT = 53;
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        private char[,] levelDataArray = new char[LEVEL_HEIGHT,LEVEL_WIDTH];
+
+        private List<Ground> grounds = new List<Ground>();
+        private List<Platform> platforms = new List<Platform>();
+        private World world;
+        private string levelData;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            world = new World(new Vector2(0, 9.82f));
         }
 
         /// <summary>
@@ -47,7 +61,55 @@ namespace Platformer
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            CreateGameComponents();
+        }
+
+        //Allows you to clear the world and create new GameComponents
+        private void CreateGameComponents()
+        {
+            // Textures
+            Texture2D groundTexture = Content.Load<Texture2D>("Images\\ground");
+            Texture2D platformTexture = Content.Load <Texture2D>("Images\\platform");
+
+            //Read in the text file
+            var stream = TitleContainer.OpenStream("Levels/level1.txt");
+            var reader = new StreamReader(stream);
+
+            for(int col = 0; col < LEVEL_HEIGHT; col++)
+            {
+                //Read in a line from level.txt
+                levelData = reader.ReadLine();
+                for (int row = 0; row < LEVEL_WIDTH; row++)
+                {
+                    levelDataArray[col,row] = levelData[row];
+                }
+            }
+            //Put the level data into a list according to the character
+            for (int col = 0; col < LEVEL_HEIGHT; col++)
+            {
+                for (int row = 0; row < LEVEL_WIDTH; row++)
+                {
+                    if (levelDataArray[col, row] == 'x')
+                    {
+                        Vector2 position = new Vector2(ConvertUnits.ToSimUnits(row),
+                                                ConvertUnits.ToSimUnits(col));
+
+                        Ground ground = new Ground(world, groundTexture, position);
+
+                        grounds.Add(ground);
+                    }
+
+                    if (levelDataArray[col, row] == 'o')
+                    {
+                        Vector2 position = new Vector2(ConvertUnits.ToSimUnits(row),
+                                                ConvertUnits.ToSimUnits(col));
+
+                        Platform platform = new Platform(world, platformTexture, position);
+
+                        platforms.Add(platform);
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -57,10 +119,6 @@ namespace Platformer
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -72,9 +130,22 @@ namespace Platformer
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Gray);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null,
+             null, Matrix.Identity);
+
+            foreach (Platform p in platforms)
+            {
+                p.Draw(spriteBatch);
+            }
+
+            foreach (Ground g in grounds)
+            {
+                g.Draw(spriteBatch);
+            }
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
--- Platformer/Platformer/Platform.cs
+++ Platformer/Platformer/Platform.cs
@@ -2,11 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using FarseerPhysics;
+using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
+using FarseerPhysics.Factories;
 
 namespace Platformer
 {
     class Platform
     {
+        public Body Body { get; private set; }
 
+        private Texture2D texture;
+
+        public Platform(World world, Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+
+            Body = BodyFactory.CreateRectangle(world,
+                ConvertUnits.ToSimUnits(texture.Width),
+                ConvertUnits.ToSimUnits(texture.Height), 1f, position);
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture,
+                ConvertUnits.ToDisplayUnits(Body.Position * 9),
+                null,
+                Color.White,
+                0f,
+                new Vector2(texture.Width / 2, texture.Height / 2),  // origin
+                1f,
+                SpriteEffects.None,
+                0f);
+        }
     }
 }
